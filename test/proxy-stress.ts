3afc8e046849d584d04ccf3102c07e89c8fa42bf
greg-vbase <greg@vbase.com>
2025-01-25 23:04:58 -0800

    Parameterized proxy and reproed TransactionNotFound stress failure

--- test/proxy-stress.ts
+++ test/proxy-stress.ts
@@ -1,8 +1,22 @@
 import { getLocal, CompletedRequest } from "mockttp";
 import { PassThroughResponse } from "mockttp/dist/rules/requests/request-handler-definitions";
 
-// Define the maximum delay (in milliseconds).
-const MAX_DELAY_MS = 5 * 1000;
+// The target (proxied) and the proxy ports.
+const TGT_PORT = 8545;
+const PROXY_PORT = 8546;
+
+// Read configuration from environment variables with defaults.
+// The maximum request or response delay (in milliseconds).
+const MAX_DELAY_MS = parseInt(process.env.MAX_DELAY_MS || "5000", 10);
+// The probability of failing eth_getTransactionReceipt with TransactionNotFound.
+// This is used to simulate tardy nodes that do not return a transaction receipt
+// for a mined transaction as Alchemy has been doing.
+const TX_NOT_FOUND_FAILURE_PROBABILITY = parseFloat(process.env.TX_NOT_FOUND_FAILURE_PROBABILITY || "0.5");
+
+console.log(`Proxy Configuration:
+  MAX_DELAY_MS: ${MAX_DELAY_MS} ms
+  TX_NOT_FOUND_FAILURE_PROBABILITY: ${TX_NOT_FOUND_FAILURE_PROBABILITY}`
+);
 
 // Define the function to add a random delay.
 const addRandomDelay = async (): Promise<void> => {
@@ -22,12 +36,51 @@ const printBody = (msg: CompletedRequest | PassThroughResponse) => {
   console.log(`Body: ${body ? JSON.stringify(JSON.parse(body), null, 2) : "No body"}`);
 };
 
+// Determine if a request is a eth_getTransactionReceipt call
+// that should fail with TransactionNotFound.
+const failWithTxNotFoundIfNecessary = (request: CompletedRequest) => {
+
+  // Parse out the request body.
+  const body = request.body?.buffer
+    ? Buffer.from(request.body.buffer).toString("utf-8")
+    : null;
+  if (!body) {
+    console.log("FailWithTxNotFoundIfNecessary: Empty body");
+    return;
+  }
+
+  let parsedBody;
+  try {
+    parsedBody = JSON.parse(body);
+  } catch (error) {
+     // If body parsing fails, do not fail the request.
+     console.log("FailWithTxNotFoundIfNecessary: Failed to parse body: " + error);
+     return;
+  }
+  // If body parsing succeeds and we have a request to fail with a given probability,
+  // fail the request.
+  if (parsedBody.method === "eth_getTransactionReceipt") {
+    console.log("FailWithTxNotFoundIfNecessary: Got a eth_getTransactionReceipt request");
+    if (Math.random() < TX_NOT_FOUND_FAILURE_PROBABILITY) {
+      console.log("FailWithTxNotFoundIfNecessary: Failing with TransactionNotFound");
+      // This is not the right way to simulate a transaction not found, but it is good enough.
+      // as all errors from eth_getTransactionReceipt are processed the same way.
+      throw {
+        name: "TransactionNotFound",
+        code: 430,
+        message: "Transaction not found",
+      };
+    }
+  }
+};
+
 // Function to process requests.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const processRequest = async (request: CompletedRequest): Promise<any> => {
   console.log(`Request: ${JSON.stringify(request)}`);
   printBody(request);
   await addRandomDelay();
+  failWithTxNotFoundIfNecessary(request);
   return request;
 };
 
@@ -44,15 +97,15 @@ const processResponse = async (response: PassThroughResponse): Promise<any> => {
 const proxy = getLocal();
 
 (async () => {
-  // Start the proxy on port 8546
-  await proxy.start(8546);
+  // Start the proxy on the proxy port.
+  await proxy.start(PROXY_PORT);
 
-  proxy.forAnyRequest().thenForwardTo("http://localhost:8545", {
+  proxy.forAnyRequest().thenForwardTo("http://localhost:" + TGT_PORT, {
     beforeRequest: processRequest,
     beforeResponse: processResponse,
   });
 
-  console.log("Proxy running on http://localhost:8546");
+  console.log("Proxy running on http://localhost:" + PROXY_PORT);
 })();
 
 process.on("SIGINT", async () => {
